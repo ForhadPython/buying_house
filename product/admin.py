ea8fe08cf44865572cb0766740f6954099d5f089
Forhad_Bappy <bappy.python@gmail.com>
2025-01-19 15:43:02 +0600

    image resize

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import ProductBanner, ProductAbout
 
+
 # Register your models here.
 
 @admin.register(ProductBanner)
@@ -9,8 +10,9 @@ class ProductBannerAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description')
     list_filter = ('title',)
 
+
 @admin.register(ProductAbout)
 class ProductAboutAdmin(admin.ModelAdmin):
-    list_display = ('title', 'image', 'short_desc', 'description', 'link')
+    list_display = ('title', 'image')
     search_fields = ('title', 'short_desc', 'description', 'link')
     list_filter = ('title',)
